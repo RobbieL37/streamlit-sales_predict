fb8c53cb5e8cc26d8e78bd2d6570322709d5727f
Xiaofeng Liang <xiaofengliang@mail.adelphi.edu>
2022-12-14 15:40:25 -0500

    Update sales_predict_interaction.py

--- sales_predict_interaction.py
+++ sales_predict_interaction.py
@@ -5,5 +5,19 @@ Date: 12/14/2022
 '''
 
 
-import streamlit
+import streamlit as st
+
+
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_container()
+model_training = st.beta_container()
+
+
+with header: 
+    st.title('Welcome to the testing website.')
+    st.text('In this project I will create an interaction with tables and charts.')
+    
+
+
 
